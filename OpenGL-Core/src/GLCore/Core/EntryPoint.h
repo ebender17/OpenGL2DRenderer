f3f4feeb5654afe08fdd5dda2822e6f4f4208339
Emily Bender <36086595+ebender17@users.noreply.github.com>
2024-06-22 12:26:44 -0400

    Logging (#1)
    * wrapper log class for spdlog
    
    * added missing GLCORE_API macro

--- OpenGL-Core/src/GLCore/Core/EntryPoint.h
+++ OpenGL-Core/src/GLCore/Core/EntryPoint.h
@@ -6,6 +6,11 @@ extern GLCore::Application* GLCore::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GLCore::Log::Init();
+	GLCORE_LOG_WARN("Initialized Log!");
+	int a = 5;
+	LOG_INFO("Hello! Var={0}", a);
+
 	auto app = GLCore::CreateApplication();
 	app->Run();
 	delete app;
