f8dc6b0437a4be4246609be03203f52bec15a5c5
Emily Bender <bender.emily1@gmail.com>
2024-09-06 21:00:14 -0400

    pop layer & overlay fix

--- OpenGL-Core/src/GLCore/Core/LayerStack.cpp
+++ OpenGL-Core/src/GLCore/Core/LayerStack.cpp
@@ -30,8 +30,8 @@ namespace GLCore {
 
     void LayerStack::PopLayer(Layer* layer)
     {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-        if (it != m_Layers.end())
+        auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+        if (it != m_Layers.begin() + m_LayerInsertIndex)
         {
             layer->OnDetach();
             m_Layers.erase(it);
@@ -41,7 +41,7 @@ namespace GLCore {
 
     void LayerStack::PopOverlay(Layer* overlay)
     {
-        auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+        auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
         if (it != m_Layers.end())
         {
             overlay->OnDetach();
