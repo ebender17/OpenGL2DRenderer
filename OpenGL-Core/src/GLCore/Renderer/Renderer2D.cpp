57d219f64cf4fe7ec4efc15968108c4d41bd65eb
Emily Bender <bender.emily1@gmail.com>
2024-09-26 22:58:05 -0400

    Added RotatedQuad primitive

--- OpenGL-Core/src/GLCore/Renderer/Renderer2D.cpp
+++ OpenGL-Core/src/GLCore/Renderer/Renderer2D.cpp
@@ -82,6 +82,7 @@ namespace GLCore {
     {
         PROFILE_FUNCTION();
 
+        s_Data->QuadShader->SetFloat("u_TilingFactor", 1.0f);
         s_Data->QuadShader->SetFloat4("u_Color", color);
         s_Data->WhiteTexture->Bind();
 
@@ -106,6 +107,49 @@ namespace GLCore {
         texture->Bind();
 
         glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+        s_Data->QuadShader->SetMat4("u_Transform", transform);
+
+        s_Data->QuadVertexArray->Bind();
+        RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
+    }
+
+    void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
+    {
+        DrawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, color);
+    }
+
+    void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
+    {
+        PROFILE_FUNCTION();
+
+        s_Data->QuadShader->SetFloat("u_TilingFactor", 1.0f);
+        s_Data->QuadShader->SetFloat4("u_Color", color);
+        s_Data->WhiteTexture->Bind();
+
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+            * glm::rotate(glm::mat4(1.0f), rotation, { 0.0f, 0.0f, 1.0f })
+            * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+        s_Data->QuadShader->SetMat4("u_Transform", transform);
+
+        s_Data->QuadVertexArray->Bind();
+        RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
+    }
+
+    void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+    {
+    }
+
+    void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+    {
+        PROFILE_FUNCTION();
+
+        s_Data->QuadShader->SetFloat("u_TilingFactor", 1.0f);
+        s_Data->QuadShader->SetFloat4("u_Color", tintColor);
+        texture->Bind();
+
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+            * glm::rotate(glm::mat4(1.0f), rotation, { 0.0f, 0.0f, 1.0f })
+            * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
         s_Data->QuadShader->SetMat4("u_Transform", transform);
 
         s_Data->QuadVertexArray->Bind();
